9dc7a6506ce8949ef0620fb97477405d847dd865
Umesh Mali <111166993+maliumesh1@users.noreply.github.com>
2023-12-25 10:15:05 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (15.73%) - LeetSync

---
+++ 91-decode-ways/decode-ways.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int numDecodings(String s) {
+         int n=s.length() ;
+        int[] dp=new int[n+1] ;
+        dp[n]=1 ;
+        for(int i = n-1 ; i >= 0 ; i--)
+            if(s.charAt(i)!='0') 
+            {
+                dp[i] = dp[i+1] ;
+                if(i < n-1 && (s.charAt(i)=='1' || s.charAt(i)=='2' && s.charAt(i+1)<'7')) 
+				    dp[i]+=dp[i+2];
+            }
+        return dp[0];   
+    }
+}
