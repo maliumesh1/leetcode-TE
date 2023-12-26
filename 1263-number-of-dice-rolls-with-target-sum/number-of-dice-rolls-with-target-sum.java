f26761d79f60b04ee7a20cfc0f81a387750c39fd
Umesh Mali <111166993+maliumesh1@users.noreply.github.com>
2023-12-26 11:46:19 +0530

    Time: 7 ms (92.53%) | Memory: 40.8 MB (61.74%) - LeetSync

--- 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.java
+++ 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.java
@@ -1,27 +1,29 @@
-import java.util.Arrays;
-
 class Solution {
-    final int mod = (int) Math.pow(10, 9) + 7;
-
     public int numRollsToTarget(int n, int k, int target) {
-        int[][] dp = new int[n + 1][target + 1];
-        for (int[] d : dp) {
-            Arrays.fill(d, -1);
-        }
-        return recursion(dp, n, k, target);
-    }
+        final int MOD = 1000000007;
 
-    private int recursion(int[][] dp, int n, int k, int target) {
-        if (target == 0 && n == 0) return 1;
-        if (n == 0 || target <= 0) return 0;
-
-        if (dp[n][target] != -1) return (int) (dp[n][target] % mod);
-        int ways = 0;
-        for (int i = 1; i <= k; i++) {
-            ways = (ways + recursion(dp, n - 1, k, target - i)) % mod;
+        // Check if it's impossible to reach the target with the given number of dice and faces
+        if (n * k < target) {
+            return 0;
         }
-        dp[n][target] = ways % mod;
-        return dp[n][target];
+
+        // Initialize a 2D array to store the number of ways to achieve each target sum using a specific number of dice
+        long[][] dp = new long[n + 1][target + 1];
+
+        // Base case: There is one way to get a sum of 0 with 0 dice
+        dp[0][0] = 1;
+
+        // Dynamic Programming: Iterate over the number of dice and target sums
+        for (int i = 1; i <= n; i++) {
+            for (int j = i; j <= Math.min(i * k, target); j++) {
+                for (int temp = 1; temp <= Math.min(k, j); temp++) {
+                    // Update the number of ways to achieve the current target sum
+                    dp[i][j] = (dp[i][j] + dp[i - 1][j - temp]) % MOD;
+                }
+            }
+        }
+
+        // Return the result, cast to integer
+        return (int) dp[n][target];
     }
 }
-
