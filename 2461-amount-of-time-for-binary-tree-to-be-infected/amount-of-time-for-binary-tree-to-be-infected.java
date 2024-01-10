8e0dbba19a5075b783350087655094c50a906dd1
Umesh Mali <111166993+maliumesh1@users.noreply.github.com>
2024-01-10 22:50:29 +0530

    Time: 143 ms (21.52%) | Memory: 119.7 MB (14.64%) - LeetSync

---
+++ 2461-amount-of-time-for-binary-tree-to-be-infected/amount-of-time-for-binary-tree-to-be-infected.java
@@ -0,0 +1,64 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private Map<Integer, List<Integer>> adjacencyList = new HashMap<>();
+
+    public int amountOfTime(TreeNode root, int start) {
+        convertToGraph(root);
+        Deque<Integer> queue = new ArrayDeque<>();
+        Set<Integer> visited = new HashSet<>();
+      
+        queue.offer(start);
+        int time = -1; 
+      
+        while (!queue.isEmpty()) {
+            time++;
+            for (int i = queue.size(); i > 0; i--) {
+                int currentNode = queue.pollFirst();
+                visited.add(currentNode);
+              
+                if (adjacencyList.containsKey(currentNode)) {
+                    for (int neighbor : adjacencyList.get(currentNode)) {
+                        if (!visited.contains(neighbor)) {
+                            queue.offer(neighbor);
+                        }
+                    }
+                }
+            }
+        }
+        return time;
+    }
+
+    private void convertToGraph(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+      
+        if (node.left != null) {
+            adjacencyList.computeIfAbsent(node.val, k -> new ArrayList<>()).add(node.left.val);
+            adjacencyList.computeIfAbsent(node.left.val, k -> new ArrayList<>()).add(node.val);
+        }
+      
+        if (node.right != null) {
+            adjacencyList.computeIfAbsent(node.val, k -> new ArrayList<>()).add(node.right.val);
+            adjacencyList.computeIfAbsent(node.right.val, k -> new ArrayList<>()).add(node.val);
+        }
+      
+        
+        convertToGraph(node.left);
+        convertToGraph(node.right);
+    }
+}
