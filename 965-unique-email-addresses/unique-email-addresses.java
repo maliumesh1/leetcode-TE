f72fe222cd3906c44c9361dfd92e4fe77c388102
Umesh Mali <111166993+maliumesh1@users.noreply.github.com>
2024-02-02 23:17:11 +0530

    Time: 16 ms (57.25%) | Memory: 45.3 MB (18.16%) - LeetSync

---
+++ 965-unique-email-addresses/unique-email-addresses.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int numUniqueEmails(String[] emails) {
+        Set<String> normalized = new HashSet<>(); // used to save simplified email address, cost O(n) sapce.
+        for (String email : emails) {
+            String[] parts = email.split("@"); // split into local and domain parts.
+            String[] local = parts[0].split("\\+"); // split local by '+'.
+            normalized.add(local[0].replace(".", "") + "@" + parts[1]); // remove all '.', and concatenate '@' and domain.        
+        }
+        return normalized.size();
+    }    
+}
