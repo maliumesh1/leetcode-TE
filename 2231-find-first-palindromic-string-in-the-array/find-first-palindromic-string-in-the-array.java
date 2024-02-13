814b166f5079e8cb54b0e792f3a10500097608bc
Umesh Mali <111166993+maliumesh1@users.noreply.github.com>
2024-02-13 09:32:03 +0530

    Time: 1 ms (100.00%) | Memory: 44.6 MB (84.10%) - LeetSync

---
+++ 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.java
@@ -0,0 +1,24 @@
+public class Solution {
+    public boolean check(String s) {
+        int i = 0, j = s.length() - 1;
+        while (i <= j) {
+            if (s.charAt(i) == s.charAt(j)) {
+                i++;
+                j--;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    public String firstPalindrome(String[] words) {
+        for (String word : words) {
+            if (check(word)) {
+                return word;
+            }
+        }
+        return "";
+    }
+}
+
