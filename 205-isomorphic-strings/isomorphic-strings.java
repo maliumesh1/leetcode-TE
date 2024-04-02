d9be53de58863be28ec7e8adedb1c9863915dce4
Umesh Mali <111166993+maliumesh1@users.noreply.github.com>
2024-04-02 08:02:56 +0530

    Time: 2 ms (99.63%) | Memory: 42.2 MB (79.50%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -0,0 +1,28 @@
+public class Solution {
+    
+    public boolean isIsomorphic(String sString, String tString) {
+
+        char[] s = sString.toCharArray();
+        char[] t = tString.toCharArray();
+
+        int length = s.length;
+        if(length != t.length) return false;
+
+        char[] sm = new char[256];
+        char[] tm = new char[256];
+
+        for(int i=0; i<length; i++){
+            char sc = s[i];
+            char tc = t[i];
+            if(sm[sc] == 0 && tm[tc] == 0){
+                sm[sc] = tc;
+                tm[tc] = sc;
+            }else{
+                if(sm[sc] != tc || tm[tc] != sc){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
