11abf3c25010a13c0c1c1fab0db9647704bcd88f
Umesh Mali <111166993+maliumesh1@users.noreply.github.com>
2024-04-02 22:22:33 +0530

    Time: 5 ms (75.38%) | Memory: 44.3 MB (13.04%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -2,9 +2,9 @@ class Solution {
     public boolean isPalindrome(int x) {
         int k=0;
         int d=x;
-        while(x>0){
-            int r = x%10;
-            k = k*10 + r;
+        while(x>0)
+        {
+            k = k*10 + x%10;
             x=x/10;
         }
         return k==d;
