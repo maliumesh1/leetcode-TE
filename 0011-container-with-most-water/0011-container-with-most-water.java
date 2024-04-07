56c8ebaeabbe6d7a0a8a9fd2d256ddafe57a9786
Umesh Mali <111166993+maliumesh1@users.noreply.github.com>
2024-04-07 14:00:38 +0530

    Time: 2 ms (99.31%), Space: 57.4 MB (78.07%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,3 +1,8 @@
+//-------------------------------------------------------------------------------------------------------------
+
+// BRUTE FORCE TC O(N^2) USING ARRAY  (TIME CONSUMING )
+//______________________________________
+
 // class Solution {
 //     public int maxArea(int[] height) {
 
@@ -17,7 +22,9 @@
 //     }
 // }
 
-
+//-------------------------------------------------------------------------------------------------------------
+// BRUTE FORCE TC O(N^2) USING ARRAYLIST   (NOT RUN IN THIS DRIVER CODE)
+//______________________________________
 // import java.util.*;
 // class Solution {
 //     public int maxArea(ArrayList<Integer> height) {
@@ -38,31 +45,90 @@
 //     }
 // }
 
+//-------------------------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------------------
+// OPTIMAL TC O(N)  USING ARRAY
+// class Solution {
+//     public int maxArea(int[] height) {
+//         int lp = 0 ;
+//         int rp = height.length-1;
+//         int maxwater=0;
 
+//         while(lp != rp)
+//         {
+//             int ht = Math.min(height[lp] , height[rp]);
+//             int width = rp-lp;
+//             int currwater = ht * width ;
+            
+//             maxwater = Math.max(currwater, maxwater);
+//             if(height[lp] < height[rp])
+//             {
+//                 lp++;
+//             }
+//             else {
+//                 rp--;
+//             }
+//         } 
+//       return maxwater;
+        
+//     }
+// }
+
+
+
+//-------------------------------------------------------------------------------------------------------------
+// OPTIMAL TC O(N)  USING ARRAY LIST (NOT RUN IN THIS DRIVER CODE)
+// class Solution {
+//     public int maxArea(ArrayList<Integer> height) {
+
+//         int lp = 0 ;
+//         int rp = height.size()-1;
+//         int maxwater=0;
+
+//         while(lp != rp)
+//         {
+//             int ht = Math.min(height.get(lp) , height.get(rp));
+//             int width = rp-lp;
+//             int currwater = ht * width ;
+            
+//             maxwater = Math.max(currwater, maxwater);
+//             if(height.get(lp) < height.get(rp))
+//             {
+//                 lp++;
+//             }
+//             else {
+//                 rp--;
+//             }
+//         } 
+//       return maxwater;
+        
+//     }
+// }
+
+//-------------------------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------------------
 
 
 class Solution {
     public int maxArea(int[] height) {
-        int lp = 0 ;
-        int rp = height.length-1;
-        int maxwater=0;
+        int left = 0;
+        int right = height.length - 1;
+        int maxWater = 0;
 
-        while(lp != rp)
-        {
-            int ht = Math.min(height[lp] , height[rp]);
-            int width = rp-lp;
-            int currwater = ht * width ;
-            
-            maxwater = Math.max(currwater, maxwater);
-            if(height[lp] < height[rp])
-            {
-                lp++;
+        while (left < right) {
+            int minHeight = Math.min(height[left], height[right]);
+            int area = minHeight * (right - left);
+            maxWater = Math.max(maxWater, area);
+
+            while (height[left] <= minHeight && left < right) {
+                left++;
             }
-            else {
-                rp--;
+
+            while (height[right] <= minHeight && left < right) {
+                right--;
             }
-        } 
-      return maxwater;
-        
+        }
+
+        return maxWater;
     }
 }
