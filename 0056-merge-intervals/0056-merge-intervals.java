a6051124d6fb909f6d6bab41641a7fcf9eb70332
Umesh Mali <111166993+maliumesh1@users.noreply.github.com>
2024-06-11 09:09:53 +0530

    Time: 5 ms (97.49%), Space: 47.5 MB (5.6%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,25 +1,66 @@
 class Solution {
-    public int[][] merge(int[][] arr) {
-        int n = arr.length;
-        // Sort the array
-        Arrays.sort(arr , (a,b) -> Integer.compare(a[0],b[0]));
-
-        int start = arr[0][0];
-        int end = arr[0][1];
-
-        ArrayList<int[]> ans=  new ArrayList<>();
-        for(int i = 1 ; i < n ;i++){
-            if(arr[i][0] <= end){
-                end = Math.max(end , arr[i][1]);
+    public int[][] merge(int[][] intervals) {
+        int[][] numbers = countingSort(intervals); 
+        Map<Integer, Integer> set = new HashMap<>();
+        int index = 0;
+        int tracker = 0; 
+        
+        for(int i = 0; i< numbers.length; i++){
+            if(numbers[i][0] <= numbers[tracker][1]){
+                if(numbers[i][1] > numbers[tracker][1]){
+                    numbers[tracker][1] = numbers[i][1];
+                }
+                index = tracker;
+            }else{
+                index = i;
+                tracker = i;
             }
-            else{
-                ans.add(new int[]{start ,end});
-                start = arr[i][0];
-                end = arr[i][1];
-
+            if(set.containsKey(numbers[index][0])){
+                int max = set.get(numbers[index][0]) <= numbers[index][1] ? numbers[index][1] : set.get(numbers[index][0]);
+                set.put(numbers[index][0], max);               
+            }else{
+                set.put(numbers[index][0], numbers[index][1]); 
             }
         }
-        ans.add(new int[]{start ,end});
-        return ans.toArray(new int[ans.size()][]);
+        
+        int[][] output = new int[set.size()][2];
+        
+        index = 0;
+        
+        for(int i = 0; i < numbers.length; i++){
+            if(set.containsKey(numbers[i][0]) && set.get(numbers[i][0]) == numbers[i][1]){
+                set.remove(numbers[i][0]);
+                output[index][0] = numbers[i][0];
+                output[index++][1] = numbers[i][1];
+            }     
+        }
+        
+        return output;
+        
+    }
+    
+    public int[][] countingSort(int[][] nums){
+        int[][] output = new int[nums.length][2];
+        int max = 0;        
+        
+        for(int[] array: nums){
+            max = max > array[0] ? max : array[0];
+        }
+        
+        int[] counts = new int[max + 1];
+        for(int[] array: nums){
+            ++counts[array[0]];
+        }
+        
+        for(int i = 1; i < counts.length; i++){
+            counts[i] += counts[i - 1];
+        }
+        
+        for(int i = nums.length - 1; i >= 0; i--){
+            output[--counts[nums[i][0]]][0] = nums[i][0];
+            output[counts[nums[i][0]]][1] = nums[i][1];
+        }
+        
+        return output;
     }
 }
