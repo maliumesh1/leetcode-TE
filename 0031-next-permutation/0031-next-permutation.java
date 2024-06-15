a7f0a14400aea93ccdb675649d17a6ce6a5ea74b
Umesh Mali <111166993+maliumesh1@users.noreply.github.com>
2024-06-16 00:11:37 +0530

    Time: 0 ms (100%), Space: 43.2 MB (8.77%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,23 +1,30 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-        int ind1=-1;
+                int ind1=-1;
         int ind2=-1;
 
-        // step 1 find breaking point 
-        for(int i=nums.length-2;i>=0;i--){
-            if(nums[i]<nums[i+1]){
+        // step 1 find breaking point . to breaking pt cha idex I as ind1 mhanun save kra.
+        for( int i=nums.length-2;  i >= 0 ; i--)
+       {
+            if(nums[i]<nums[i+1])
+             {
                 ind1=i;
                 break;
             }
         }
-        // if there is no breaking  point 
+
+        // if there is no breaking  point . jr breaking pt nasel tr arr rev kra…
         if(ind1==-1){
             reverse(nums,0);
         }
         
-        else{
+        else
+         {    // JR  breaking pt asel tr 
             // step 2 find next greater element and swap with ind2
-            for(int i=nums.length-1;i>=0;i--){
+
+            for(int i=nums.length-1;i>=0;i--)
+           {	
+// JO BP CHYA NNANTR MOTHE ELE ASEL TO IND 2 MADHE SAVE KRA..
                 if(nums[i]>nums[ind1]){
                     ind2=i;
                     break;
@@ -25,6 +32,7 @@ class Solution {
             }
 
             swap(nums,ind1,ind2);
+
             // step 3 reverse the rest right half
             reverse(nums,ind1+1);
         }
@@ -39,10 +47,12 @@ class Solution {
     void reverse(int[] nums,int start){
         int i=start;
         int j=nums.length-1;
+
         while(i<j){
             swap(nums,i,j);
             i++;
             j--;
         }
+
     }
 }
