849669672eadb2d3d523c64c999ac9120c5f5930
Umesh Mali <111166993+maliumesh1@users.noreply.github.com>
2024-06-16 01:43:40 +0530

    Time: 8 ms (85.59%), Space: 46.5 MB (31.88%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,66 +1,27 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        int[][] numbers = countingSort(intervals); 
-        Map<Integer, Integer> set = new HashMap<>();
-        int index = 0;
-        int tracker = 0; 
-        
-        for(int i = 0; i< numbers.length; i++){
-            if(numbers[i][0] <= numbers[tracker][1]){
-                if(numbers[i][1] > numbers[tracker][1]){
-                    numbers[tracker][1] = numbers[i][1];
-                }
-                index = tracker;
-            }else{
-                index = i;
-                tracker = i;
+        if (intervals.length <= 1) return intervals;
+
+        Arrays.sort(intervals, (arr1, arr2) -> Integer.compare(arr1[0], arr2[0]));
+
+        List < int[] > output_arr = new ArrayList < > ();
+        int[] current_interval = intervals[0];
+        output_arr.add(current_interval);
+
+        for (int[] interval: intervals) {
+            int current_begin = current_interval[0];
+            int current_end = current_interval[1];
+            int next_begin = interval[0];
+            int next_end = interval[1];
+
+            if (current_end >= next_begin) {
+                current_interval[1] = Math.max(current_end, next_end);
+            } else {
+                current_interval = interval;
+                output_arr.add(current_interval);
             }
-            if(set.containsKey(numbers[index][0])){
-                int max = set.get(numbers[index][0]) <= numbers[index][1] ? numbers[index][1] : set.get(numbers[index][0]);
-                set.put(numbers[index][0], max);               
-            }else{
-                set.put(numbers[index][0], numbers[index][1]); 
-            }
         }
-        
-        int[][] output = new int[set.size()][2];
-        
-        index = 0;
-        
-        for(int i = 0; i < numbers.length; i++){
-            if(set.containsKey(numbers[i][0]) && set.get(numbers[i][0]) == numbers[i][1]){
-                set.remove(numbers[i][0]);
-                output[index][0] = numbers[i][0];
-                output[index++][1] = numbers[i][1];
-            }     
-        }
-        
-        return output;
-        
-    }
-    
-    public int[][] countingSort(int[][] nums){
-        int[][] output = new int[nums.length][2];
-        int max = 0;        
-        
-        for(int[] array: nums){
-            max = max > array[0] ? max : array[0];
-        }
-        
-        int[] counts = new int[max + 1];
-        for(int[] array: nums){
-            ++counts[array[0]];
-        }
-        
-        for(int i = 1; i < counts.length; i++){
-            counts[i] += counts[i - 1];
-        }
-        
-        for(int i = nums.length - 1; i >= 0; i--){
-            output[--counts[nums[i][0]]][0] = nums[i][0];
-            output[counts[nums[i][0]]][1] = nums[i][1];
-        }
-        
-        return output;
+        return output_arr.toArray(new int[output_arr.size()][]);
+
     }
 }
