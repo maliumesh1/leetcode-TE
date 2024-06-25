c2d1835365eae86254e916ba448354cb418d97a3
Umesh Mali <111166993+maliumesh1@users.noreply.github.com>
2024-06-25 18:51:24 +0530

    Time: 0 ms (100%), Space: 41.1 MB (44.06%) - LeetHub

--- 1114-binary-search-tree-to-greater-sum-tree/1114-binary-search-tree-to-greater-sum-tree.java
+++ 1114-binary-search-tree-to-greater-sum-tree/1114-binary-search-tree-to-greater-sum-tree.java
@@ -15,13 +15,14 @@
  */
 class Solution {
         private int sum = 0;
-            public TreeNode bstToGst(TreeNode root) {
-                    if (root == null) return null;
-                            bstToGst(root.right);
-                                    sum += root.val;
-                                            root.val = sum;
-                                                    bstToGst(root.left);
-                                                            return root;
-                                                                }
+            public TreeNode bstToGst(TreeNode root)
+            {
+             if (root == null) return null;
+             bstToGst(root.right);
+             sum += root.val;  
+             root.val = sum;
+             bstToGst(root.left); 
+            return root;
+ }
                                                                 
 }
