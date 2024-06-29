56a33f0120fd4521881277905b76bc0f56707398
Umesh Mali <111166993+maliumesh1@users.noreply.github.com>
2024-06-30 01:03:28 +0530

    Time: 2 ms (85.55%), Space: 44.3 MB (84.84%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.java
@@ -0,0 +1,30 @@
+class Solution {
+        public List<List<Integer>> combinationSum(int[] candidates, int target) {
+                List<List<Integer>> list= new ArrayList<List<Integer>>();
+                        find(list,candidates,target,new ArrayList<Integer>(),0);
+                                return list;
+                                    }
+                                        
+                                            
+                                                private void find(List<List<Integer>> list,int[] candidates,int target,ArrayList<Integer> temp,int i)
+                                                    {
+                                                            if(target==0)
+                                                                    {
+                                                                                list.add(new ArrayList<Integer>(temp));
+                                                                                            return;
+                                                                                                    }
+                                                                                                            if(i==candidates.length)
+                                                                                                                       return;
+                                                                                                                               
+                                                                                                                                       // pick
+                                                                                                                                               if(candidates[i]<=target){
+                                                                                                                                                           temp.add(candidates[i]);
+                                                                                                                                                                       find(list,candidates,target-candidates[i],temp,i);
+                                                                                                                                                                                   temp.remove(temp.size()-1);
+                                                                                                                                                                                           }
+
+                                                                                                                                                                                                   // not pick
+                                                                                                                                                                                                           find(list,candidates,target,temp,i+1);
+                                                                                                                                                                                                               }
+                                                                                                                                                                                                               
+}
