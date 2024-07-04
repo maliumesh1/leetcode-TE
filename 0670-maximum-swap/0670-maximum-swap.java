e7b38b1df5b2adf61890a35913d4b03e5cad2eb7
Umesh Mali <111166993+maliumesh1@users.noreply.github.com>
2024-07-04 12:36:36 +0530

    Time: 0 ms (100%), Space: 40.1 MB (83.6%) - LeetHub

---
+++ 0670-maximum-swap/0670-maximum-swap.java
@@ -0,0 +1,20 @@
+class Solution {
+        public int maximumSwap(int num) {
+                int x = num;
+                        int delta = 0;
+                                int hi = x % 10, multiple = 1, prev = 1;
+                                        x /= 10;
+                                                while (x > 0) {
+                                                            multiple *= 10;
+                                                                        if (x % 10 > hi) {
+                                                                                        hi = x % 10;
+                                                                                                        prev = multiple;
+                                                                                                                    } else if (x % 10 < hi) {
+                                                                                                                                    delta = (hi - x % 10) * multiple - (hi - x % 10) * prev;
+                                                                                                                                                }
+                                                                                                                                                            x /= 10;
+                                                                                                                                                                    }
+                                                                                                                                                                            return num + delta;
+                                                                                                                                                                                }
+                                                                                                                                                                                
+}
