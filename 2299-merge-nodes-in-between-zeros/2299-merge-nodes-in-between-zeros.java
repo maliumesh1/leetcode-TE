22f9d6f7ef8f439d1777cb9d86de9bbd6842e01d
Umesh Mali <111166993+maliumesh1@users.noreply.github.com>
2024-07-04 10:07:26 +0530

    Time: 4 ms (100%), Space: 82.6 MB (31.62%) - LeetHub

--- 2299-merge-nodes-in-between-zeros/2299-merge-nodes-in-between-zeros.java
+++ 2299-merge-nodes-in-between-zeros/2299-merge-nodes-in-between-zeros.java
@@ -1,19 +1,19 @@
 class Solution {
         public ListNode mergeNodes(ListNode head) {
                 // 'curr` pointer to traverse the list
-                        ListNode curr = head.next;
-                                // 'updater` pointer points to the node to be updated
-                                        ListNode updater = head;
-                                                // 'sum` variable to store the sum
-                                                        int sum = 0;
+          ListNode curr = head.next;
+       // 'updater` pointer points to the node to be updated
+           ListNode updater = head;
+           // 'sum` variable to store the sum
+             int sum = 0;
 
-                                                                while(curr != null){
-                                                                            if(curr.val == 0){
-                                                                                            // updating the node value
-                                                                                                            updater.val = sum;
-                                                                                                                            if(curr.next != null){
-                                                                                                                                                // it is not the end of the list so 
-                                                                                                                                                                    updater = updater.next;
+              while(curr != null){
+                if(curr.val == 0){
+                  // updating the node value
+                 updater.val = sum;
+                  if(curr.next != null){
+                // it is not the end of the list so 
+                   updater = updater.next;
                                                                                                                                                                                     }else{
                                                                                                                                                                                                         // we reached the end of the list
                                                                                                                                                                                                                             updater.next = null;
