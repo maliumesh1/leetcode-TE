4de51925e014a063bf4c284f60d4f80a55fdc599
Umesh Mali <111166993+maliumesh1@users.noreply.github.com>
2024-07-30 11:53:19 +0530

    Time: 18 ms (99.16%), Space: 45.7 MB (9.47%) - LeetHub

--- 1756-minimum-deletions-to-make-string-balanced/1756-minimum-deletions-to-make-string-balanced.java
+++ 1756-minimum-deletions-to-make-string-balanced/1756-minimum-deletions-to-make-string-balanced.java
@@ -1,16 +1,15 @@
 class Solution {
     public int minimumDeletions(String s) {
-        int n = s.length();
-        int[] f = new int[n + 1];
-        int b = 0;
-        for (int i = 1; i <= n; ++i) {
-            if (s.charAt(i - 1) == 'b') {
-                f[i] = f[i - 1];
-                ++b;
-            } else {
-                f[i] = Math.min(f[i - 1] + 1, b);
+        int ans = 0;
+        int count = 0;
+        for (char x : s.toCharArray()) {
+            if (x == 'b') {
+                count += 1;
+            } else if (count > 0) {
+                ans += 1;
+                count -= 1;
             }
         }
-        return f[n];
+        return ans;
     }
 }
