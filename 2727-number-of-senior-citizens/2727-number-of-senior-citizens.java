abbab2c38f4e26353825cab6c0ec64ad44f0ba41
Umesh Mali <111166993+maliumesh1@users.noreply.github.com>
2024-08-01 18:42:10 +0530

    Time: 0 ms (100%), Space: 42 MB (89.84%) - LeetHub

---
+++ 2727-number-of-senior-citizens/2727-number-of-senior-citizens.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int countSeniors(String[] details) {
+        int count = 0;
+        for (String detail : details) {
+            int age = (detail.charAt(11) - '0') * 10 + (detail.charAt(12) - '0');
+            if (age > 60) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+}
