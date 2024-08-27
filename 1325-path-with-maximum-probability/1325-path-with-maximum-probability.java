70ea2bda51a228908ab68250f2846cebf0d04fd0
Umesh Mali <111166993+maliumesh1@users.noreply.github.com>
2024-08-27 21:13:43 +0530

    Time: 10 ms (98.38%), Space: 56.8 MB (71.14%) - LeetHub

---
+++ 1325-path-with-maximum-probability/1325-path-with-maximum-probability.java
@@ -0,0 +1,33 @@
+class Solution {
+    public double maxProbability(int n, int[][] edges, double[] succProb, int start_node, int end_node) {
+
+        double[] shortPath = new double[n];
+
+        shortPath[start_node] = 1;
+
+        for (int i = 0; i < n - 1; i++) {
+            boolean isUpdated = false;
+            for (int j = 0; j < edges.length; j++) {
+                int u = edges[j][0];
+                int v = edges[j][1];
+                double wt = succProb[j];
+
+                if (shortPath[u] * wt > shortPath[v]) {
+                    shortPath[v] = shortPath[u] * wt;
+                    isUpdated = true;
+                }
+
+                if (shortPath[v] * wt > shortPath[u]) {
+                    shortPath[u] = shortPath[v] * wt;
+                    isUpdated = true;
+                }
+            }
+            if (!isUpdated)
+                break;
+        }
+
+        return shortPath[end_node];
+
+    }
+
+}
