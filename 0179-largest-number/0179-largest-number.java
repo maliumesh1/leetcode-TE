8c9bd1389b1f291e8271d1ff337e96e9d1b7df4a
Umesh Mali <111166993+maliumesh1@users.noreply.github.com>
2024-09-18 22:14:11 +0530

    Time: 6 ms (54.52%), Space: 43 MB (76.31%) - LeetHub

--- 0179-largest-number/0179-largest-number.java
+++ 0179-largest-number/0179-largest-number.java
@@ -1,21 +1,19 @@
 class Solution {
-    public String largestNumber(int[] nums) {
-        int n = nums.length;
-        // convert the integer array to string array
-        String[] arr = new String[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = String.valueOf(nums[i]);
-        }
-        Arrays.sort(arr, (a, b) -> (b + a).compareTo(a + b));
-        if (arr[0].equals("0")) {
-            return "0";
+        public String largestNumber(int[] nums) {
+                StringBuilder s=new StringBuilder();
+                        String str[]=new String[nums.length];
+                                for(int i=0;i<nums.length;i++){
+                                            str[i]=String.valueOf(nums[i]);
+                                                    }
+                                                            Arrays.sort(str,(a,b)->(b+a).compareTo(a+b));
 
-        }
-        StringBuilder result = new StringBuilder();
-        for (String num : arr) {
-            result.append(num);
-        }
-        return result.toString();
-    }
-
+                                                                    if(str[0].equals("0")){
+                                                                                return "0";
+                                                                                        }
+                                                                                                for(String num : str){
+                                                                                                            s.append(num);
+                                                                                                                    }
+                                                                                                                            return s.toString();
+                                                                                                                                }
+                                                                                                                                
 }
