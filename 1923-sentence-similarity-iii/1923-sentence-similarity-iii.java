f80c1b0eebd00f30986b13d2a555bf4720a42b9d
Umesh Mali <111166993+maliumesh1@users.noreply.github.com>
2024-10-06 21:34:07 +0530

    Time: 0 ms (100%), Space: 41.2 MB (98.41%) - LeetHub

---
+++ 1923-sentence-similarity-iii/1923-sentence-similarity-iii.java
@@ -0,0 +1,36 @@
+class Solution {
+        public boolean areSentencesSimilar(String sent1, String sent2) {
+                String first = sent1;
+                        String sec = sent2;
+                                if(first.length()==sec.length()){
+                                            return first.equals(sec);
+                                                    }
+                                                            if(sent2.length()<first.length()){
+                                                                        first = sent2;
+                                                                                    sec = sent1;
+                                                                                            }
+                                                                                                    int i = -1;
+                                                                                                          while(i+1<first.length() && first.charAt(i+1)==sec.charAt(i+1)){
+                                                                                                                  i++;
+                                                                                                                        }
+                                                                                                                                int j = first.length();
+                                                                                                                                        int last = sec.length();
+                                                                                                                                              while(j-1>=0 && first.charAt(j-1)==sec.charAt(last-1)){
+                                                                                                                                                      j--;
+                                                                                                                                                              last--;
+                                                                                                                                                                    }
+
+                                                                                                                                                                          if(i==first.length()-1 && sec.charAt(i+1)==' '){
+                                                                                                                                                                                  return true;
+                                                                                                                                                                                        } 
+                                                                                                                                                                                              if(j==0 && sec.charAt(last-1)==' '){
+                                                                                                                                                                                                      return true;
+                                                                                                                                                                                                            }
+                                                                                                                                                                                                                   if(i+1>=j && sec.charAt(i)==' ' && sec.charAt(last)==' '){
+                                                                                                                                                                                                                               return true;
+                                                                                                                                                                                                                                      }
+                                                                                                                                                                                                                                             
+                                                                                                                                                                                                                                                   return false;
+                                                                                                                                                                                                                                                       }
+                                                                                                                                                                                                                                                       
+}
