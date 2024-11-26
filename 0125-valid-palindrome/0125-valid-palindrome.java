71c349f36ec45e5df6aaafe22a99fec81ee53fa6
Umesh Mali <111166993+maliumesh1@users.noreply.github.com>
2024-11-27 01:36:33 +0530

    Time: 1 ms (100%), Space: 43.3 MB (52.27%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,13 +1,23 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        // s.toCharArray
-        s = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
-        int n = s.length();
-        for (int i = 0; i < n / 2; i++) {
-            // Compare characters from both ends
-            if (s.charAt(i) != s.charAt(n - i - 1)) {
+        char[] arr = s.toCharArray();
+        int i = 0;
+        int j = arr.length - 1;
+        while (i < j) {
+            while ((i <= arr.length - 1) && !((arr[i] >= 'a' && arr[i] <= 'z') || (arr[i] >= 'A' && arr[i] <= 'Z')
+                    || (arr[i] >= '0' && arr[i] <= '9')))
+                i++;
+            while ((j >= 0) && !((arr[j] >= 'a' && arr[j] <= 'z') || (arr[j] >= 'A' && arr[j] <= 'Z')
+                    || (arr[j] >= '0' && arr[j] <= '9')))
+                j--;
+            if (i >= j)
+                break;
+            if (arr[i] >= 'A' && arr[i] <= 'Z')
+                arr[i] += 'a' - 'A';
+            if (arr[j] >= 'A' && arr[j] <= 'Z')
+                arr[j] += 'a' - 'A';
+            if (arr[i++] != arr[j--])
                 return false;
-            }
         }
         return true;
     }
