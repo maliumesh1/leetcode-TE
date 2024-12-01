67a13072c707ed0e472a65e3e8814572f868eae3
Umesh Mali <111166993+maliumesh1@users.noreply.github.com>
2024-12-02 01:09:01 +0530

    Time: 2 ms (99.38%), Space: 48.4 MB (14.21%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,29 +1,174 @@
+// class Solution {
+//     public List<Integer> majorityElement(int[] nums) {
+//         ArrayList<Integer> al = new ArrayList<>();
+
+//         int n=nums.length ;
+//         int min=  n/3;
+
+//         for(int i=0;i<n;i++)
+//         {
+
+//             if(al.size()==0 || al.get(0) != nums[i])
+//             {
+//                 int c=0;
+
+//                for(int j=0;j<n;j++)
+//                {
+//                   if(nums[i] == nums[j])
+//                    {
+//                       c++;
+//                    }
+//                }
+
+//                   if(c > min)
+//                    {
+//                       al.add(nums[i]);
+//                    }
+
+//                    if (al.size() == 2) break;
+//             }
+//         }
+//         return al;
+//     }
+// }
+
+// class Solution {
+//     public List<Integer> majorityElement(int[] nums) {
+//      HashMap<Integer,Integer>hm = new HashMap<>();
+//       ArrayList<Integer> al = new ArrayList<>();
+//         int n = nums.length ;
+//         int min =  n/3;
+
+//      for(int i=0;i<n;i++)
+//      {
+//         hm.put(nums[i], hm.getOrDefault(nums[i], 0)+1);
+//      }
+
+//         // Check for majority elements
+//         for (int key : hm.keySet()) 
+//         {
+//             if (hm.get(key) > min) {
+//                 al.add(key);
+//             }
+//         }
+
+//     return al;
+
+//     }
+// }
+
+// class Solution {
+//     public List<Integer> majorityElement(int[] nums) {
+//         ArrayList<Integer> al = new ArrayList<>();
+
+//         int n=nums.length ;
+//         int min=  n/3;
+
+//         for(int i=0;i<n;i++)
+//         {
+
+//             if(al.size()==0 || al.get(0) != nums[i])
+//             {
+//                 int c=0;
+
+//                for(int j=0;j<n;j++)
+//                {
+//                   if(nums[i] == nums[j])
+//                    {
+//                       c++;
+//                    }
+//                }
+
+//                   if(c > min)
+//                    {
+//                       al.add(nums[i]);
+//                    }
+
+//                    if (al.size() == 2) break;
+//             }
+//         }
+//         return al;
+//     }
+// }
+
+// class Solution {
+//     public List<Integer> majorityElement(int[] nums) {
+//      HashMap<Integer,Integer>hm = new HashMap<>();
+//       ArrayList<Integer> al = new ArrayList<>();
+//         int n = nums.length ;
+//         int min =  n/3;
+
+//      for(int i=0;i<n;i++)
+//      {
+//         hm.put(nums[i], hm.getOrDefault(nums[i], 0)+1);
+//      }
+
+//         // Check for majority elements
+//         for (int key : hm.keySet()) 
+//         {
+//             if (hm.get(key) > min) {
+//                 al.add(key);
+//             }
+//         }
+
+//     return al;
+
+//     }
+// }
+
 class Solution {
-public static List<Integer> majorityElement(int []nums) {
-        int n = nums.length; //size of the array
-        List<Integer> ls = new ArrayList<>(); // list of answers
+    public List<Integer> majorityElement(int[] nums) {
+        int candidate1 = Integer.MIN_VALUE;
+        int candidate2 = Integer.MIN_VALUE;
+        int count1 = 0;
+        int count2 = 0;
 
-        for (int i = 0; i < n; i++) {
-            //selected element is v[i]:
-            // Checking if v[i] is not already
-            // a part of the answer:
-            if (ls.size() == 0 || ls.get(0) != nums[i]) {
-                int cnt = 0;
-                for (int j = 0; j < n; j++) {
-                    // counting the frequency of v[i]
-                    if (nums[j] == nums[i]) {
-                        cnt++;
-                    }
-                }
+        for (int num : nums) {
+            if (num == candidate1) {
+                count1++;
 
-                // check if frquency is greater than n/3:
-                if (cnt > (n / 3))
-                    ls.add(nums[i]);
+            } else if (num == candidate2) {
+                count2++;
+
+            } else if (count1 == 0) {
+                candidate1 = num;
+                count1 = 1;
+
+            } else if (count2 == 0) {
+                candidate2 = num;
+                count2 = 1;
+
+            } else {
+                count1--;
+                count2--;
             }
-
-            if (ls.size() == 2) break;
         }
 
-        return ls;
+        // Second pass: kiti candidate aahet he bghate.
+        count1 = 0;
+        count2 = 0;
+
+        for (int num : nums) {
+            if (num == candidate1) {
+                count1++;
+            } else if (num == candidate2) {
+                count2++;
+            }
+        }
+
+        // Collect the majority elements
+        List<Integer> result = new ArrayList<>();
+        int n = nums.length;
+        if (count1 > n / 3) {
+            result.add(candidate1);
+        }
+        if (count2 > n / 3) {
+            result.add(candidate2);
+        }
+
+        return result;
     }
 }
+
+// TC - N
+// SC - 1
