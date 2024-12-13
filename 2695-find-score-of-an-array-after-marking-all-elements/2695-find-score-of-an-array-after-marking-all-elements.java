235f0d972cb6a3a3e3f08bbc3b636833dbac99a7
Umesh Mali <111166993+maliumesh1@users.noreply.github.com>
2024-12-13 21:23:16 +0530

    Time: 2 ms (100%), Space: 59.7 MB (51.39%) - LeetHub

---
+++ 2695-find-score-of-an-array-after-marking-all-elements/2695-find-score-of-an-array-after-marking-all-elements.java
@@ -0,0 +1,16 @@
+class Solution {
+    public long findScore(int[] nums) {
+        long res = 0;
+        for (int i = 0; i < nums.length; i += 2) {
+            int start = i;
+            while (i + 1 < nums.length && nums[i + 1] < nums[i]) {
+                i++;
+            }
+            for (int j = i; j >= start; j -= 2) {
+                res += nums[j];
+            }
+        }
+        return res;
+    }
+
+}
